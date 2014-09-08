bb481aa5a35b9867cf25ca9ed0e2d820c9cbb1ac
Tyson Van Buren <tysonvanburen444@gmail.com>
2014-09-07 20:13:41 -0600

    BLARG

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-ActionMailer::Base.delivery_method = :sendmail
+# ActionMailer::Base.delivery_method = :sendmail
 
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
