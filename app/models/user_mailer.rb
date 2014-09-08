513ee820d4b309638d5cdb1c4c3d6be1fb89d949
Tyson Van Buren <tysonvanburen444@gmail.com>
2014-09-07 18:40:39 -0600

    deletec user_mailer

--- app/models/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-  def welcome_email(user)
-    recipients    user.email
-    from          "My Awesome Site Notifications <tysonvanburen@gmail.com>"
-    subject       "Welcome to My Awesome Site"
-    sent_on       Time.now
-    body          {:user => user, :url => "http://example.com/login"}
-  end
-end
